ba5b3ec48e5fd5aeb8886149f117b87a89cc295c
darshank17 <109973284+darshank17@users.noreply.github.com>
2024-03-19 20:32:01 +0000

    My First Code in js

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,8 @@
+const accountId = 4638339
+let accountEmail = "darshankotecha17@gmail.com"
+let accountPass = "darshan3393808"
+accountCity = " Pune "
+let accountState ;
+
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
